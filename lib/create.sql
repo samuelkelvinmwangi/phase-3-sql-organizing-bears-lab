a7ff89f0fd740733d149ddaa4b265dfc2898dfd5
samuel <samuelkelvinmwangi@gmail.com>
2023-07-23 03:23:05 +0300

    finishing the bear lab

---
+++ lib/create.sql
@@ -0,0 +1,8 @@
+CREATE TABLE bears (
+  id INTEGER PRIMARY KEY,
+  age INTEGER,
+  sex TEXT,
+  color TEXT,
+  temparament TEXT,
+  alive BOOLEAN,
+);
---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears(name, age, sex, color, temparament, alive) VALUES ('Mr.Chocolate', 3, 'M', 'Blue', 'Happy', 'False');
+INSERT INTO bears(name, age, sex, color, temparament, alive) VALUES ('Rowdy', 7, 'M', 'Indigo', 'sad', 'True');
+INSERT INTO bears(name, age, sex, color, temparament, alive) VALUES ('Tabitha', 1, 'F', 'Yellow', 'Annoyed', 'True');
+INSERT INTO bears(name, age, sex, color, temparament, alive) VALUES ('Seagent Brown', 9, 'M', 'Brown', 'Jovial', 'True');
+INSERT INTO bears(name, age, sex, color, temparament, alive) VALUES ('Melissa', 8, 'F', 'Pink', 'Moody', 'False');
+INSERT INTO bears(name, age, sex, color, temparament, alive) VALUES ('Grinch',2, 'M', 'Black', 'Jumpy', 'True');
+INSERT INTO bears(name, age, sex, color, temparament, alive) VALUES ('Wendy', 4, 'F', 'red', 'Loud', 'False');
+INSERT INTO bears(name, age, sex, color, temparament, alive) VALUES ('null', 6, 'M', 'Green', 'Silent', 'True');
